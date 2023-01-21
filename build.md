4c732e761eabde0cdfaefba1a93cb8f8ece7ca05
Christian Karrié <christian@karrie.info>
2023-01-21 20:48:22 +0100

    push v0.0.3

--- build.md
+++ build.md
@@ -18,14 +18,32 @@ cd ~/workspace/venvs/homeassistant-oekofen
 source bin/activate
 cd ~/workspace/src/oekofen-api
 ```
-### github Release erstellen
 
-### Versionsnummer anpassen in setup.py
+### Push git changes
+- Mit git changes pushen
+
+### github Release erstellen
+- https://github.com/ckarrie/oekofen-api/releases/new
+- Tag (create new): v0.0.2
+- Title: v0.0.2
+- Button "Publish Release"
+
+### Anpassen in setup.py
 ```
 nano setup.py
 ```
+```python
+setup(
+    ...
+    version="0.0.2",
+    download_url='https://github.com/ckarrie/oekofen-api/archive/refs/tags/v0.0.2.tar.gz',
+    ...
+)
 
 ```
+
+```
+rm -rf dist
 python3 -m build
 ```
 
--- setup.py
+++ setup.py
@@ -5,15 +5,19 @@ with open("README.md", "r") as f:
 
 setup(
     name="oekofen_api",
-    version="0.0.2",
+    version="0.0.3",
     author="Christian Karrié",
     author_email="ckarrie@gmail.com",
     description="A python library to retrieve statistics from your Oekofen Pelletronic",
-    download_url='https://github.com/ckarrie/oekofen-api/archive/refs/tags/v0.0.2.tar.gz',
+    download_url='https://github.com/ckarrie/oekofen-api/archive/refs/tags/v0.0.3.tar.gz',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ckarrie/oekofen-api",
     packages=find_packages(),
+    install_requires=[
+        'aiohttp>=3.8.1',
+        'voluptuous>=0.13.1'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
